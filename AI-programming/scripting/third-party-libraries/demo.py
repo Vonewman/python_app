edd62a3c21c68b656350bffd7435438abdf595d4
vonewman <vonewman7@gmail.com>
2020-09-06 17:56:39 -0400

    add third-party-library/

---
+++ AI-programming/scripting/third-party-libraries/demo.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import pytz
+
+
+utc = pytz.utc
+ist = pytz.timezone('Asia/Kolkata')
+
+now = datetime.now(tz=utc)
+ist_now = now.astimezone(ist)
+
+
+print(now)
+print(ist_now)
