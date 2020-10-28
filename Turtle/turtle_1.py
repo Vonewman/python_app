66893cf08f88f90ec4e628f36fa13961c2f37a7f
vonewman <vonewman7@gmail.com>
2020-10-28 11:33:01 -0400

    add some changes

--- Turtle/turtle_1.py
+++ Turtle/turtle_1.py
@@ -2,7 +2,7 @@ from turtle import *
 
 forward(100)   # Move forward
 left(90)       # Turn 90 degrees left
-forward(50)    
+forward(50)
 width(5)       # Width of the pencil
 forward(100)
 color('red')
