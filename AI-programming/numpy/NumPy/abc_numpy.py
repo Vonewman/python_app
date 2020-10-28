6bbb94ce0a1ad2a379fddfeae118339b52a87be2
vonewman <vonewman7@gmail.com>
2020-10-28 18:17:59 -0400

    add numpy/

---
+++ AI-programming/numpy/NumPy/abc_numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Create numpy array of letters a-j
+letter_array = np.array(['a','b','c','d','e','f','g','h','i'])
+
+print("letter Array: ", letter_array)
+
+print("Dtype: ", letter_array.dtype)
+print("Shape: ", letter_array.shape)
+print("Size: ",  letter_array.size)
